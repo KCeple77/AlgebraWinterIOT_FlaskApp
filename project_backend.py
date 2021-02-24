82278f66b8b5e8deb94faacb873d50c4b697b8ad
Kristijan Ceple <kristijan.ceple@fer.hr>
2021-02-24 21:39:00 +0100

    auto-push

---
+++ project_backend.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+from flask import Flask
+from flask import jsonify
+from flask_mysqldb import MySQL
+from flask import request
+from datetime import datetime
+
+app = Flask(__name__)
+mysql = MySQL()
+
+app.config['MYSQL_USER'] = 'telemetryuser'
+app.config['MYSQL_PASSWORD'] = 'TelemetryUser123.'
+app.config['MYSQL_DB'] = 'telemetryDB'
+app.config['MYSQL_HOST'] = 'localhost'
+mysql.init_app(app)
+
+
+@app.route('/api/telemetry/get?<string:begin_point_str>&<string:end_point_str>', methods=['GET'])
+def return_books_sql(begin_point_str, end_point_str):
+    conn = mysql.connect
+    cursor = conn.cursor()
+
+    cmd = 'SELECT * FROM Measurement JOIN Device WHERE (CreatedOn BETWEEN %s AND %s);'
+    params = [begin_point_str, end_point_str]
+
+    cursor.execute(cmd, params)
+    rows = cursor.fetchall()
+    return jsonify({'All telemetry data': rows})
+
+
+@app.route('/api/telemetry/post', methods=['POST'])
+def add_book():
+    data_piece = request.get_json()
+    conn = mysql.connect
+    cursor = conn.cursor()
+
+    # SensorName and SensorValue
+    cmd = "INSERT INTO Measurement (DeviceId, SensorName, SensorValue, CreatedOn) VALUES ((SELECT DeviceId FROM Device WHERE %s = Name), %s, %s, NOW());"
+    params = (data_piece['SensorName'], data_piece['SensorValue'])
+
+    cursor.execute(cmd, params)
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return "200"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80, debug=True)
