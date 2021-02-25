665f66c49587fc5a703ff3b116ba63241c7db965
Kristijan Ceple <kristijan.ceple@fer.hr>
2021-02-25 19:54:14 +0100

    auto-push

--- project_backend.py
+++ project_backend.py
@@ -27,34 +27,40 @@ mysql.init_app(app)
 
 
 @app.route('/api/telemetry/get/<string:begin_point_str>&<string:end_point_str>', methods=['GET'])
-def return_books_sql(begin_point_str, end_point_str):
-    conn = mysql.connect
-    cursor = conn.cursor()
+def return_measurements(begin_point_str, end_point_str):
+    try:
+        conn = mysql.connect
+        cursor = conn.cursor()
 
-    cmd = 'SELECT * FROM Measurement JOIN Device WHERE (CreatedOn BETWEEN %s AND %s);'
-    params = [begin_point_str, end_point_str]
+        cmd = 'SELECT * FROM Measurement JOIN Device WHERE (CreatedOn BETWEEN %s AND %s);'
+        params = [begin_point_str, end_point_str]
 
-    cursor.execute(cmd, params)
-    rows = cursor.fetchall()
+        cursor.execute(cmd, params)
+        rows = cursor.fetchall()
 
-    return jsonify({'All telemetry data': rows})
+        return jsonify({'All telemetry data': rows})
+    except Exception as e:
+        print("Error: unabel to fetch items", e)
 
 
 @app.route('/api/telemetry/post', methods=['POST'])
-def add_book():
-    data_piece = request.get_json()
-    conn = mysql.connect
-    cursor = conn.cursor()
+def add_measurement():
+    try:
+        data_piece = request.get_json()
+        conn = mysql.connect
+        cursor = conn.cursor()
 
-    # SensorName and SensorValue
-    cmd = "INSERT INTO Measurement (DeviceId, SensorName, SensorValue, CreatedOn) VALUES ((SELECT DeviceId FROM Device WHERE %s = Name), %s, %s, NOW());"
-    params = [data_piece['SensorName'], data_piece['SensorName'], data_piece['SensorValue']]
+        # SensorName and SensorValue
+        cmd = "INSERT INTO Measurement (DeviceId, SensorName, SensorValue, CreatedOn) VALUES ((SELECT DeviceId FROM Device WHERE %s = Name), %s, %s, NOW());"
+        params = [data_piece['SensorName'], data_piece['SensorName'], data_piece['SensorValue']]
 
-    cursor.execute(cmd, params)
-    conn.commit()
-    cursor.close()
-    conn.close()
-    return "200"
+        cursor.execute(cmd, params)
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return "200"
+    except Exception as e:
+        print("Error: unable to fetch items", e)
 
 
 if __name__ == "__main__":
